dba7b4d71acc74ed3a17215aad5e89b86a143a40
nbhaohao <nbhaohao@163.com>
2021-05-03 10:01:52 +0800

    init:线性查找

---
+++ src/LinearSearch/LinearSearch.java
@@ -0,0 +1,15 @@
+package LinearSearch;
+
+public class LinearSearch {
+    private LinearSearch() {
+    }
+
+    public static <T> int search(T[] data, T target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].equals(target)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
